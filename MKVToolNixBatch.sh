00c39a097654555543880e906dcd2eff76ff7d57
Peter <43998116+PetercSnk@users.noreply.github.com>
2024-06-01 17:43:27 +0100

    Update MKVToolNixBatch.sh

--- MKVToolNixBatch.sh
+++ MKVToolNixBatch.sh
@@ -9,7 +9,7 @@ do
     inpath="$indir/$filename"
     outpath="$outdir/$filename"
     # insert the MKVToolNix command:
-    /usr/bin/mkvmerge --priority lower --output "$outpath" --subtitle-tracks 2 --no-attachments --language 0:ja --track-name '0:[Judas] x265 10b' --display-dimensions 0:1920x1080 --language 1:ja --track-name 1:Japanese --sub-charset 2:UTF-8 --language 2:en --track-name 2:English --default-track-flag 2:yes '(' "$input" ')' --attachment-name OpenSansCondensedMedium.ttf --attachment-mime-type font/ttf --attach-file /home/vboxuser/Share/fonts/OpenSansCondensedMedium.ttf --track-order 0:0,0:1,0:2
+    
     # replace output with $outpath      :   --output "$outpath"
     # $inpath goes within the braces    :   '(' "$inpath" ')'
 done
